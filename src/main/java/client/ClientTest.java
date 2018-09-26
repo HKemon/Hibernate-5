fcb2a21bed9e58f4c6c4f8bd6488334b68e19ec1
emon <hk.emon.007@gmail.com>
2018-09-26 13:04:34 +0600

    save emplyee entity

--- src/main/java/client/ClientTest.java
+++ src/main/java/client/ClientTest.java
@@ -1,16 +1,29 @@
 package client;
 
+import entities.Employee;
 import org.hibernate.Session;
 import util.HibernateUtil;
+
+import java.util.Date;
 
 public class ClientTest {
     public static void main(String[] args) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            String sql = "select version()";
-            String result = (String) session.createNativeQuery(sql).getSingleResult();
-            System.out.println(result);
+            Employee employee = getEmployee();
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Employee getEmployee(){
+        Employee employee = new Employee();
+        employee.setEmployeeName("Emon");
+        employee.setEmail("emon@gmail.com");
+        employee.setDob(new Date());
+        employee.setSalary(2000.0);
+        return employee;
     }
 }
---
+++ src/main/java/entities/Employee.java
@@ -0,0 +1,61 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="employee_table")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Integer employeeId;
+
+    @Column(name = "employee_name")
+    private String employeeName;
+
+    @Column(name = "employee_email")
+    private String email;
+
+    @Column(name = "employee_dob")
+    private Date dob;
+
+    @Column(name = "employee_salary")
+    private Double salary;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
